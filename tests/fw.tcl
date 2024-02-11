2479d53f7bfba0e453dcb088110783deb55246d6
Tomasz Hemperek <tomasz.hemperek@dectris.com>
2024-02-11 16:53:24 +0100

    Add axi stream to test_fw

--- tests/fw.tcl
+++ tests/fw.tcl
@@ -7,46 +7,30 @@ set design_name fw
 create_bd_design $design_name
 current_bd_design $design_name
 
-
 ##################################################################
 # DESIGN PROCs
 ##################################################################
 
 
-
-# Procedure to create entire design; Provide argument to make
-# procedure reusable. If parentCell is "", will use root.
+# Procedure to create entire design
 proc create_root_design { parentCell } {
 
-  variable script_folder
-  variable design_name
-
-  if { $parentCell eq "" } {
-     set parentCell [get_bd_cells /]
-  }
-
-  # Get object for parentCell
-  set parentObj [get_bd_cells $parentCell]
-  if { $parentObj == "" } {
-     catch {common::send_gid_msg -ssname BD::TCL -id 2090 -severity "ERROR" "Unable to find parent cell <$parentCell>!"}
-     return
-  }
-
-  # Make sure parentObj is hier blk
-  set parentType [get_property TYPE $parentObj]
-  if { $parentType ne "hier" } {
-     catch {common::send_gid_msg -ssname BD::TCL -id 2091 -severity "ERROR" "Parent <$parentObj> has TYPE = <$parentType>. Expected to be <hier>."}
-     return
-  }
-
-  # Save current instance; Restore later
-  set oldCurInst [current_bd_instance .]
-
-  # Set parent object as current
-  current_bd_instance $parentObj
-
-
   # Create interface ports
+  set AXIS_RX [ create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:axis_rtl:1.0 AXIS_RX ]
+  set_property -dict [ list \
+   CONFIG.HAS_TKEEP {0} \
+   CONFIG.HAS_TLAST {1} \
+   CONFIG.HAS_TREADY {1} \
+   CONFIG.HAS_TSTRB {0} \
+   CONFIG.LAYERED_METADATA {undef} \
+   CONFIG.TDATA_NUM_BYTES {4} \
+   CONFIG.TDEST_WIDTH {0} \
+   CONFIG.TID_WIDTH {0} \
+   CONFIG.TUSER_WIDTH {0} \
+   ] $AXIS_RX
+
+  set AXIS_TX [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:axis_rtl:1.0 AXIS_TX ]
+
   set S_AXI [ create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:aximm_rtl:1.0 S_AXI ]
   set_property -dict [ list \
    CONFIG.ADDR_WIDTH {15} \
@@ -89,9 +73,26 @@ proc create_root_design { parentCell } {
    CONFIG.POLARITY {ACTIVE_HIGH} \
  ] $areset
 
+  # Create instance: smartconnect_0, and set properties
+  set smartconnect_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:smartconnect:1.0 smartconnect_0 ]
+  set_property -dict [list \
+    CONFIG.NUM_MI {2} \
+    CONFIG.NUM_SI {1} \
+  ] $smartconnect_0
+
+
   # Create instance: axi_bram_ctrl_0, and set properties
   set axi_bram_ctrl_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_bram_ctrl:4.1 axi_bram_ctrl_0 ]
   set_property CONFIG.PROTOCOL {AXI4LITE} $axi_bram_ctrl_0
+
+
+  # Create instance: axi_fifo_mm_s_0, and set properties
+  set axi_fifo_mm_s_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_fifo_mm_s:4.2 axi_fifo_mm_s_0 ]
+  set_property -dict [list \
+    CONFIG.C_USE_RX_DATA {1} \
+    CONFIG.C_USE_TX_CTRL {0} \
+    CONFIG.C_USE_TX_DATA {1} \
+  ] $axi_fifo_mm_s_0
 
 
   # Create instance: blk_mem_gen_0, and set properties
@@ -108,20 +109,23 @@ proc create_root_design { parentCell } {
 
 
   # Create interface connections
-  connect_bd_intf_net -intf_net S_AXI_0_1 [get_bd_intf_ports S_AXI] [get_bd_intf_pins axi_bram_ctrl_0/S_AXI]
+  connect_bd_intf_net -intf_net AXI_STR_RXD_0_1 [get_bd_intf_ports AXIS_RX] [get_bd_intf_pins axi_fifo_mm_s_0/AXI_STR_RXD]
+  connect_bd_intf_net -intf_net S_AXI_1 [get_bd_intf_ports S_AXI] [get_bd_intf_pins smartconnect_0/S00_AXI]
   connect_bd_intf_net -intf_net axi_bram_ctrl_0_BRAM_PORTA [get_bd_intf_pins axi_bram_ctrl_0/BRAM_PORTA] [get_bd_intf_pins blk_mem_gen_0/BRAM_PORTA]
   connect_bd_intf_net -intf_net axi_bram_ctrl_0_BRAM_PORTB [get_bd_intf_pins axi_bram_ctrl_0/BRAM_PORTB] [get_bd_intf_pins blk_mem_gen_0/BRAM_PORTB]
+  connect_bd_intf_net -intf_net axi_fifo_mm_s_0_AXI_STR_TXD [get_bd_intf_ports AXIS_TX] [get_bd_intf_pins axi_fifo_mm_s_0/AXI_STR_TXD]
+  connect_bd_intf_net -intf_net smartconnect_0_M00_AXI [get_bd_intf_pins axi_bram_ctrl_0/S_AXI] [get_bd_intf_pins smartconnect_0/M00_AXI]
+  connect_bd_intf_net -intf_net smartconnect_0_M01_AXI [get_bd_intf_pins axi_fifo_mm_s_0/S_AXI] [get_bd_intf_pins smartconnect_0/M01_AXI]
 
   # Create port connections
   connect_bd_net -net aresetn_1 [get_bd_ports areset] [get_bd_pins util_vector_logic_0/Op1]
-  connect_bd_net -net s_axi_aclk_0_1 [get_bd_ports aclk] [get_bd_pins axi_bram_ctrl_0/s_axi_aclk]
-  connect_bd_net -net util_vector_logic_0_Res [get_bd_pins axi_bram_ctrl_0/s_axi_aresetn] [get_bd_pins util_vector_logic_0/Res]
+  connect_bd_net -net s_axi_aclk_0_1 [get_bd_ports aclk] [get_bd_pins axi_bram_ctrl_0/s_axi_aclk] [get_bd_pins axi_fifo_mm_s_0/s_axi_aclk] [get_bd_pins smartconnect_0/aclk]
+  connect_bd_net -net util_vector_logic_0_Res [get_bd_pins axi_bram_ctrl_0/s_axi_aresetn] [get_bd_pins axi_fifo_mm_s_0/s_axi_aresetn] [get_bd_pins smartconnect_0/aresetn] [get_bd_pins util_vector_logic_0/Res]
 
   # Create address segments
+  assign_bd_address -offset 0x00000000 -range 0x00001000 -target_address_space [get_bd_addr_spaces S_AXI] [get_bd_addr_segs axi_bram_ctrl_0/S_AXI/Mem0] -force
+  assign_bd_address -offset 0x00001000 -range 0x00001000 -target_address_space [get_bd_addr_spaces S_AXI] [get_bd_addr_segs axi_fifo_mm_s_0/S_AXI/Mem0] -force
 
-
-  # Restore current instance
-  current_bd_instance $oldCurInst
 
   validate_bd_design
   save_bd_design
@@ -135,10 +139,8 @@ proc create_root_design { parentCell } {
 
 create_root_design ""
 
-set wrapper_file [make_wrapper -files [get_files -filter {FILE_TYPE == "Block Designs"}] -top]
-add_files -norecurse $wrapper_file
+set wrapper_file [make_wrapper -files [get_files -filter {FILE_TYPE == "Block Designs"}] -top -import]
 set_property top fw_wrapper [current_fileset -simset]
-
 
 update_compile_order -fileset [current_fileset]
 set_property -name {xsim.elaborate.xelab.more_options} -value {-dll} -objects [current_fileset -simset]
--- tests/test_fw.py
+++ tests/test_fw.py
@@ -47,7 +47,7 @@ cocotb.triggers.FallingEdge = OnFallingSignal
 cocotb.triggers.RisingEdge = OnRisingSignal
 
 from cocotbext.axi import AxiLiteBus, AxiLiteMaster
-
+from cocotbext.axi import AxiStreamSink, AxiStreamSource, AxiStreamBus
 
 async def clock(signal, timer):
     signal.setimmediatevalue(0)
@@ -66,10 +66,14 @@ async def reset(signal, timer):
 
 @cocotb.test()
 async def cocotb_fw_test(dut):
+    AXIS_FIFO_BASEADDR = 0x1000
+
     cocotb.start_soon(clock(dut.aclk, Timer(100, "ns")))
     cocotb.start_soon(reset(dut.areset, Timer(520, "ns")))
 
     axil_master = AxiLiteMaster(AxiLiteBus.from_prefix(dut, "S_AXI"), dut.aclk, dut.areset)
+    axis_rx = AxiStreamSource(AxiStreamBus.from_prefix(dut, "AXIS_RX"), dut.aclk, dut.areset)
+    axis_tx = AxiStreamSink(AxiStreamBus.from_prefix(dut, "AXIS_TX"), dut.aclk, dut.areset)
 
     data_in = list(range(32))
     await axil_master.write(0x10, data_in)
@@ -80,6 +84,39 @@ async def cocotb_fw_test(dut):
 
     assert data_in == data_out
 
+    #
+    
+    rx_data_send = []
+    for i in range(4):
+        d = list(range((i+1)*4))
+        rx_data_send += d
+        await axis_rx.write(d)
+        await axis_rx.wait()
+
+    # rx_data = range(10)
+    rx_size = list((await axil_master.read(AXIS_FIFO_BASEADDR + 0x1C, 4)).data)
+    assert rx_size == [10,0,0,0]
+
+    rx_data = []
+    for _ in range(10):
+        rx_data += list((await axil_master.read(AXIS_FIFO_BASEADDR + 0x20, 4)).data)
+
+    assert rx_data == rx_data_send
+
+    #
+
+    for i in range(8):
+        await axil_master.write(AXIS_FIFO_BASEADDR + 0x10, list(range(i*4,i*4+4)))
+        await axil_master.wait()
+
+    await axil_master.write(AXIS_FIFO_BASEADDR + 0x14, [0x20,0,0,0])
+
+    # tx_size = list((await axil_master.read(AXIS_FIFO_BASEADDR + 0xC, 4)).data)
+    # print("tx_size", tx_size)
+
+    tx_data = (await axis_tx.recv()).tdata
+    assert bytearray(range(8*4)) == tx_data
+
     dut.areset.value = 0
 
 
@@ -87,11 +124,11 @@ def test_fw():
     src_path = pathlib.Path(__file__).parent.absolute()
 
     if not os.path.exists("xsim.dir/fw_wrapper_behav/xsimk.so"):
-        subprocess.run(["vivado", "-nolog", "-mode", "tcl", "-source", src_path / "fw.tcl"])
+        # subprocess.run(["vivado", "-nolog", "-mode", "tcl", "-source", src_path / "fw.tcl"])
         subprocess.run(["xvlog", "-prj", "fw/fw.sim/sim_1/behav/xsim/fw_wrapper_vlog.prj"])
         subprocess.run(["xvhdl", "-prj", "fw/fw.sim/sim_1/behav/xsim/fw_wrapper_vhdl.prj"])
         subprocess.run(
-            "xelab -L xil_defaultlib -L axi_bram_ctrl_v4_1_7 -L blk_mem_gen_v8_4_5 -L unisims_ver -L util_vector_logic_v2_0_2 -L unimacro_ver -L secureip -L xpm --snapshot fw_wrapper_behav xil_defaultlib.fw_wrapper xil_defaultlib.glbl -dll".split()
+            "xelab -L xil_defaultlib -L axi_bram_ctrl_v4_1_7 -L axi_lite_ipif_v3_0_4 -L lib_pkg_v1_0_2 -L fifo_generator_v13_2_7 -L lib_fifo_v1_0_16 -L axi_fifo_mm_s_v4_2_9 -L blk_mem_gen_v8_4_5 -L xlconstant_v1_1_7 -L lib_cdc_v1_0_2 -L proc_sys_reset_v5_0_13 -L smartconnect_v1_0 -L axi_infrastructure_v1_1_0 -L axi_register_slice_v2_1_27 -L axi_vip_v1_1_13 -L util_vector_logic_v2_0_2 -L uvm -L xilinx_vip -L unisims_ver -L unimacro_ver -L secureip -L xpm --snapshot fw_wrapper_behav xil_defaultlib.fw_wrapper xil_defaultlib.glbl -dll".split()
         )
     run(module="test_fw", xsim_design="xsim.dir/fw_wrapper_behav/xsimk.so", top_level_lang="verilog")
 
